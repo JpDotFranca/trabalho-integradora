a2f81a7da7c35ec95cdbf5ca48d6940be8ddf16a
jpdotfranca <jpdotfranca@gmail.com>
2020-10-28 20:14:54 -0300

    up

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'login',
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  public MostrarFormularioLogin: boolean = false;
+
+  public MostrarFormularioLoginClick(): void {
+    this.MostrarFormularioLogin = true;
+  }
 
 
 }

f6cec3825b46397545fcc677320cecc3c2c7e0d3
jpdotfranca <jpdotfranca@gmail.com>
2020-11-01 19:44:23 -0300

    up

---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent {
+
+
+}
